c07b6ce4da142bd5e3ff394a275f44052d514d7f
Heiner Behrends <heinerbehrends@hotmail.com>
2023-09-08 12:13:54 +0200

    Move server.ts back to root dir

--- server.ts
+++ server.ts
@@ -1,8 +1,14 @@
+// server.ts
 export default {
   // This function is called when a client connects to the server.
   async onConnect(connection, room, context) {
     // You can send messages to the client using the connection object
-    connection.send("hello from room: " + room.id);
+    connection.send(
+      JSON.stringify({
+        type: "randomLetters",
+        letters: "dsfsdfs",
+      })
+    );
     console.log(`Connection URL: ${context.request.url}`);
 
     // You can also listen for messages from the client
@@ -16,19 +22,9 @@ export default {
         [connection.id] // in this case, we exclude the client that sent the message
       );
     });
-    connection.addEventListener("updateLetters", (evt) => {
-      console.log("updateLetters"); // "hello from client (id)"
-
-      // You can also broadcast messages to all clients in the room
-      room.broadcast(
-        `message from client: ${evt.data} (id: ${connection.id}")`,
-        // You can exclude any clients from the broadcast
-        [connection.id] // in this case, we exclude the client that sent the message
-      );
-    });
   },
   // optionally, you can respond to HTTP requests as well
-  async onRequest(request, room) {
-    return new Response("hello from room: " + room.id);
-  },
+  // async onRequest(request, room) {
+  //   return new Response("hello from room: " + room.id);
+  // },
 };
