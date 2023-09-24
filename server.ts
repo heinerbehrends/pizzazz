8b1bd181340523aaf1aa76ee18c8fdefe392528f
Heiner Behrends <heinerbehrends@hotmail.com>
2023-09-24 21:17:55 +0200

    Remove stuff from server

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import type { Party, PartyConnection, PartyKitServer } from "partykit/server";
+import type { PartyKitServer } from "partykit/server";
 import { interpret } from "xstate";
 import { serverMachine } from "./src/srcServer/serverMachine";
 import { ServerToClientMessage, UserDisconnectedEvent } from "./server.types";
@@ -8,14 +8,10 @@ const serverService = interpret(serverMachine()).start();
 
 export default {
   async onConnect(connection, room) {
-    console.log("onConnect room", room);
-    const connectionId = connection.id;
-    serverService.send({ type: "newPlayer", connectionId });
-    handleIdleConnected({
-      serverService,
-      connection,
-      room,
-    });
+    serverService.send({ type: "newPlayer", connectionId: connection.id });
+    if ([...room.getConnections()].length === 1) {
+      serverService.send({ type: "firstUserConnected" });
+    }
     // listen for client messages
     connection.addEventListener("message", (event) => {
       if (typeof event.data !== "string") {
@@ -23,7 +19,7 @@ export default {
       }
       const message: ClientToServerMessage = JSON.parse(event.data);
       console.log("clientToServerMessage: ", message.type);
-      serverService.send({ ...message, connectionId });
+      serverService.send({ ...message, connectionId: connection.id });
     });
 
     // send machine events to the client
@@ -34,70 +30,15 @@ export default {
       const event = evt as ServerToClientMessage;
       room.broadcast(JSON.stringify(event), event?.excludedPlayers);
     });
+
+    connection.addEventListener("close", () => {
+      if ([...room.getConnections()].length === 0) {
+        serverService.send("lastUserDisconnected");
+      }
+      serverService.send({
+        type: "userDisconnected",
+        connectionId: connection.id,
+      } satisfies UserDisconnectedEvent);
+    });
   },
 } satisfies PartyKitServer;
-
-type ServerMessageTypeArray = Array<ServerToClientMessage["type"]>;
-
-type SendToClientArgs = {
-  toSender?: ServerMessageTypeArray;
-  toEveryoneElse?: ServerMessageTypeArray;
-  toEveryone?: ServerMessageTypeArray;
-  event: ServerToClientMessage;
-  connection: PartyConnection;
-  room: Party;
-};
-
-function reply(room: Party, connection: PartyConnection) {
-  return Array.from(room.getConnections())
-    .filter((bla: PartyConnection) => bla.id !== connection.id)
-    .map((connection) => connection.id);
-}
-
-function sendToClient({
-  toEveryone,
-  toSender,
-  toEveryoneElse,
-  event,
-  connection,
-  room,
-}: SendToClientArgs) {
-  if (toEveryone?.includes(event.type)) {
-    console.log("to everyone");
-    room.broadcast(JSON.stringify(event));
-  }
-  if (toSender?.includes(event.type)) {
-    console.log("to sender");
-    room.broadcast(JSON.stringify(event), reply(room, connection));
-  }
-  if (toEveryoneElse?.includes(event.type)) {
-    console.log("to everyone else", connection.id);
-    room.broadcast(JSON.stringify(event), [connection.id]);
-  }
-}
-
-type HandleIdleConnectedArgs = {
-  connection: PartyConnection;
-  room: Party;
-  serverService: typeof serverService;
-};
-
-function handleIdleConnected({
-  connection,
-  room,
-  serverService,
-}: HandleIdleConnectedArgs) {
-  if ([...room.getConnections()].length === 1) {
-    serverService.send({ type: "userConnected" });
-  }
-  connection.addEventListener("close", () => {
-    console.log("onClose");
-    if ([...room.getConnections()].length === 0) {
-      serverService.send("lastUserDisconnected");
-    }
-    serverService.send({
-      type: "userDisconnected",
-      connectionId: connection.id,
-    } as UserDisconnectedEvent);
-  });
-}
