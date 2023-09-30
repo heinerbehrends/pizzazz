f0bfb17384bcbe0c0ebe92e7a6e097d72308f642
Heiner Behrends <heinerbehrends@hotmail.com>
2023-09-30 20:47:08 +0200

    Change algorithm to create one dict file instead of two

--- src/srcServer/dictionaryScripts/createDictWithSortedKeys.ts
+++ src/srcServer/dictionaryScripts/createDictWithSortedKeys.ts
@@ -2,40 +2,46 @@
 import * as R from "remeda";
 // @ts-ignore-error
 import fs from "fs";
+import dictionary from "../stateServer/dictionary.json";
 
 const wordListFile = fs.readFileSync("./wordList.txt");
 
-const sortABC = (word: string) =>
+export const sortABC = (word: string) =>
   R.pipe(
     word,
     (word) => word.split(""),
     R.sort((a, b) => a.localeCompare(b)),
     R.join("")
   );
+
+const toSortedKeysDictionary = (
+  dict: Record<string, Record<string, string>>,
+  string: string
+) => {
+  const resultDict = dict;
+  const sortedString = sortABC(string);
+  const previousEntry = resultDict[sortedString];
+  resultDict[sortedString] = previousEntry
+    ? {
+        [string]: dictionary[string as keyof typeof dictionary],
+        ...previousEntry,
+      }
+    : { [string]: dictionary[string as keyof typeof dictionary] };
+  return resultDict;
+};
 // @ts-ignore-error
-const parseLimit7 = (wordListFile: Buffer) =>
+const generateDictionary = (wordListFile: Buffer) =>
   R.pipe(
     wordListFile,
     (buffer) => buffer.toString() as string,
     (string) => string.split("\n"),
     R.map((string) => string.substring(0, string.indexOf(" "))),
-    R.reject((el) => el.length > 7 || el.length === 0)
+    R.reject((el) => el.length > 7 || el.length === 0),
+    R.reduce(toSortedKeysDictionary, {})
   );
 
-const wordList = parseLimit7(wordListFile);
-
-const toSortedKeysDict = (dict: Record<string, string[]>, string: string) => {
-  const resultDict = dict;
-  const sortedString = sortABC(string);
-  const previousEntry = resultDict[sortedString];
-  resultDict[sortedString] = previousEntry
-    ? [string, ...previousEntry]
-    : [string];
-  return resultDict;
-};
-
-const associativeDictionary = wordList.reduce(toSortedKeysDict, {});
+const associativeDictionary = generateDictionary(wordListFile);
 const content = JSON.stringify(associativeDictionary);
 
-// this file will not run properly when type is set to 'module' in package.json
 fs.writeFile("./associated.json", content, console.error);
+console.log("File written successfully");
